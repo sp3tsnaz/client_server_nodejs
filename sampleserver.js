34965f60d5d4bb5ac4feffdd8c057f1f64ca4679
mohsin <mohsin>
2013-12-03 03:02:12 +0500

    First commit

---
+++ sampleserver.js
@@ -0,0 +1,22 @@
+
+var net = require('net');
+var server = net.createServer(function(c) { //'connection' listener
+  console.log('server connected');
+
+  c.on('end', function() {
+    console.log('server disconnected');    
+  });
+
+  c.on('data', function(data) {
+    console.log(data.toString());
+  }); 
+ 
+  console.log(c.address().address + "\r\n");	
+  c.write("**" + (new Date).getTime().toString() + " is the server time**\r\n");
+  c.pipe(c);   
+  
+});
+server.listen(8124, function() { //'listening' listener
+  console.log('server bound');
+});
+
